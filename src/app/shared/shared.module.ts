34c92f5e94470406ae840f535910f0704e2adf31
supatsara <supatsara.rodratsa@gmail.com>
2022-11-03 11:39:43 +0100

    update node-module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,10 @@ import { SafeHtmlPipe } from "./pipe/safeHTMLPipe.pipe";
 import { CardComponent } from './card/card.component';
 import { TabCardComponent } from './tab-card/tab-card.component';
 @NgModule({
-  declarations: [NavBarComponent, CarouselComponent, SafeHtmlPipe, CardComponent, TabCardComponent],
-  imports: [CommonModule, HttpClientModule],
-  exports: [NavBarComponent, CarouselComponent, SafeHtmlPipe, CardComponent, TabCardComponent],
-  entryComponents: [],
-  providers: [HttpClient, SafeHtmlPipe]
+    declarations: [NavBarComponent, CarouselComponent, SafeHtmlPipe, CardComponent, TabCardComponent],
+    imports: [CommonModule, HttpClientModule],
+    exports: [NavBarComponent, CarouselComponent, SafeHtmlPipe, CardComponent, TabCardComponent],
+    providers: [HttpClient, SafeHtmlPipe]
 })
 export class SharedModule {
   static forRoot(): any[] {
